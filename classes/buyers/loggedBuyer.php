ad988ca6036b35c1463159c34c58b06e2938e47a
Barbarubia <dnl.desimone@gmail.com>
2022-05-02 18:33:49 +0200

    creata classe per utente registrato

---
+++ classes/buyers/loggedBuyer.php
@@ -0,0 +1,30 @@
+<?php
+include_once __DIR__ . '/buyer.php';
+
+class LoggedBuyer extends Buyer
+{
+    // Attributi addizionali per la classe LoggedBuyer
+    private $dateOfBirth;   // Data di nascita del cliente
+    protected $password;    // Password del cliente
+
+    // Costruttore
+    public function __construct($name, $surname, $address, $houseNumber, $postalCode , $city, $email, $password)
+    {
+        parent::__construct($name, $surname, $address, $houseNumber, $postalCode , $city, $email);
+        $this->password = $password;
+    }
+
+    // Getters & Setters per gli attributi addizionali
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setDateOfBirth($dateOfBirth)
+    {
+        return $this->dateOfBirth = $dateOfBirth;
+    }
+
+    // Applicazione di uno sconto del 20% per i clienti registrati
+    protected $discount = 20;
+}
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include_once __DIR__ . '/classes/products/cloth.php';
 include_once __DIR__ . '/classes/products/food.php';
 include_once __DIR__ . '/classes/products/health.php';
 include_once __DIR__ . '/classes/products/toy.php';
+include_once __DIR__ . '/classes/buyers/loggedBuyer.php';
 
 /*
 esercizio di oggi:
@@ -20,22 +21,30 @@ Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibil
 // PROVA
 $cassetta = new Accessory('Gatti', 'Cassetta per lettiera mod.03', 5, '19,99€');
 $cassetta->setDescription('Cassetta per lettiera gatti di dimensioni L60 x P40 x H25 cm');
+var_dump($cassetta);
 
 $pettorina = new Cloth('Cani', 'Pettorina mod.09', 3, '14,99€', 'Small');
 $pettorina->setDescription('Pettorina per cani di piccola taglia');
+var_dump($pettorina);
 
 $croccantini = new Food('Cani', 'CroccanBau Manzo 80g', 10, '1,09€', '31-12-2022');
 $croccantini->setDescription('Confezione da 80g di croccantini al manzo per cani');
+var_dump($croccantini);
 
 $antiparassitario = new Health('Cani', 'Viapulciezecche Spray 500ml', 15, '9,99€', '15-09-2023');
 $antiparassitario->setDescription('Spray antiparassitario per cani in bottiglia da 500ml');
+var_dump($antiparassitario);
 
 $frisbee = new Toy('Cani', 'Frisbee mod.B4', 15, '3,99€');
 $frisbee->setDescription('Frisbee giocattolo per cani diametro 20cm');
-
-
-var_dump($cassetta);
-var_dump($pettorina);
-var_dump($croccantini);
-var_dump($antiparassitario);
 var_dump($frisbee);
+
+// Test utente non registrato
+$buyer1 = new Buyer('Pinco', 'Pallino', 'Via dei Programmatori', 99, '00185', 'Roma', 'pincopallino@mail.it');
+var_dump($buyer1);
+
+// Test utente registrato
+$buyer2 = new LoggedBuyer('Tal', 'Dei Tali', 'Piazza Sviluppatore', 10, '00199', 'Roma', 'taldeitali@mail.it', '12345678');
+$buyer2->setDateOfBirth('19-12-1986');
+$buyer2->setPhone(3331234567);
+var_dump($buyer2);
